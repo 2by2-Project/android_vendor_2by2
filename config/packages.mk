129389ee425444687a69ed9b2e3ac382b88157cf
lahaina <nattolecats@gmail.com>
2023-09-26 21:07:36 +0900

    build: Add quick tap permissions and flag Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -9,11 +9,11 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/com.android.launcher3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.launcher3.xml \
     frameworks/base/data/etc/com.android.dialer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.dialer.xml \
     frameworks/base/data/etc/com.android.contacts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.android.contacts.xml \
-    vendor/2by2/config/permissions/privapp-permissions-lineage.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineage.xml
+    vendor/2by2/prebuilts/permissions/privapp-permissions-lineage.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-lineage.xml
 
 # For Google Photos
 PRODUCT_COPY_FILES += \
-    vendor/2by2/config/permissions/com.google.android.apps.photos.NEXUS_PRELOAD.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.google.android.apps.photos.NEXUS_PRELOAD.xml
+    vendor/2by2/prebuilts/sysconfig/com.google.android.apps.photos.NEXUS_PRELOAD.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/com.google.android.apps.photos.NEXUS_PRELOAD.xml
 
 # 2byTricks
 PRODUCT_PACKAGES += \
@@ -50,4 +50,10 @@ endif
 # Use Matlog for userdebug and eng builds
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += MatLog
+endif
+
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/2by2/prebuilts/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
 endif
